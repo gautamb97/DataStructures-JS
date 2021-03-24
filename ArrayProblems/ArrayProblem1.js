70ef3a5ff239fd985a7283871f838a255e2779bf
gautamb97 <www.gautam971997@gmail.com>
2021-03-24 18:43:54 +0530

    Added:Found second smallest number from the array without sorting

--- ArrayProblems/ArrayProblem1.js
+++ ArrayProblems/ArrayProblem1.js
@@ -1,7 +1,7 @@
 //Generate 10 Random 3 digit number and stored into an array
 let random = new Array;
 for(let dieRoll = 0; dieRoll < 10; dieRoll++){
-    let randomValue = Math.floor(Math.random() * 999 + 10);
+    let randomValue = Math.floor(Math.random() *(999-100+1)+100);
     random.push(randomValue);
 }
 console.log("The ten random values are : "+random);
@@ -20,3 +20,18 @@ for (let i = 0; i < random.length; i++){
 }
 
 console.log("The second largest number is : "+secondLargestNumber);
+
+//finding second smallest number from the array without sorting
+let smallestNumber = random[0];
+let secondSmallestNumber = random[0];
+
+for (let i = 0; i < random.length; i++){
+    if (random[i] < smallestNumber){
+        secondSmallestNumber = smallestNumber;
+        smallestNumber = random[i];
+    } else if (random[i] > smallestNumber && random[i] < secondSmallestNumber){
+        secondSmallestNumber = random[i];
+    }
+}
+
+console.log("The second smallest number is : "+secondSmallestNumber);
